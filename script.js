2767ea48f9c38bed50c6aea454db7a8892a11a05
Rajesh Khadka <rajeshkhadkaofficial45@gmail.com>
2022-04-12 08:45:09 +0545

    localstorage implementation

---
+++ script.js
@@ -0,0 +1,40 @@
+let STRING = {
+  storage_name: "item",
+};
+let addBtn = document.querySelector(".add_icon");
+addBtn.addEventListener("click", () => {
+  const name = prompt("Enter the name ");
+  const link = prompt("Enter the URL ");
+  if (!link || !name) {
+    return;
+  }
+  const info = {
+    id: Date.now(),
+    name,
+    link,
+    icon: name.split("")[0].toUpperCase(),
+  };
+  saveToStorate(info, STRING.storage_name);
+  deleteFromStorage(id, STRING.storage_name);
+});
+
+// save to localstorage
+function saveToStorate(info, identifier) {
+  if (localStorage.getItem(identifier) == null) {
+    localStorage.setItem(identifier, JSON.stringify([]));
+  }
+  let oldData = JSON.parse(localStorage.getItem(identifier));
+  oldData.push(info);
+  localStorage.setItem(identifier, JSON.stringify(oldData));
+}
+
+//delete from LocalStorage
+function deleteFromStorage(id, identifier) {
+  if (localStorage.getItem(identifier) == null) {
+    return;
+  }
+  let data = JSON.parse(localStorage.getItem(identifier));
+  let deletedData = data.filter((item) => {
+    return item.id !== id;
+  });
+}
